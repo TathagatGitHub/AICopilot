1374ddd1da796ec38de50606ef9f8ff39d249ab9
Tathagat Dwivedi <tathagat.dwivedi@OceanMediainc.com>
2024-09-15 13:36:42 -0700

    Add project files.

---
+++ AICopilot/Program.cs
@@ -0,0 +1,38 @@
+using AICopilot.Components;
+using Azure.Identity;
+using Microsoft.SemanticKernel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+//Add serice for kernal
+builder.Services.AddKernel();
+
+var aiConfig = builder.Configuration.GetSection("AIConfig");
+
+builder.Services.AddAzureOpenAIChatCompletion(
+    deploymentName: aiConfig["DeploymentName"]!,
+    endpoint:aiConfig["Endpoint"]!,
+    "7c15ae1a4a8640bdbaad741285c822bb");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
